fa85db577fda0f7de92e42dc466054b502d63749
Jithin482 <jithinlal482@gmail.com>
2025-03-07 19:55:16 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,3 +73,8 @@ resource "aws_instance" "my_server" {
     Project     = "terraform-assignment"
   }
 }
+
+# Output the Public IP of the EC2 Instance
+output "public_ip" {
+  value = aws_instance.my_server.public_ip
+}
