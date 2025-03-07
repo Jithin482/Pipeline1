849400b9d8285554832ba1377efdb58633af6174
Jithin482 <jithinlal482@gmail.com>
2025-03-07 16:02:35 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket"  # Replace with your S3 bucket name
+    key    = "path/to/terraform.tfstate"  # Replace with your desired path
+    region = "eu-west-1"                  # Replace with your bucket's region
+  }
+}
+
 provider "aws" {
   region     = "eu-west-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-}
-
-resource "aws_instance" "my_server" {
-  ami           = "ami-03fd334507439f4d1"  # Replace with your desired AMI
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.pubsubnet1.id
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
-  associate_public_ip_address = true
-  tags = {
-    Name = "Terraform1"
-  }
 }
 
 resource "aws_vpc" "my_vpc" {
@@ -52,5 +49,16 @@ resource "aws_security_group" "my_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_server" {
+  ami           = "ami-03fd334507439f4d1"  # Replace with your desired AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.pubsubnet1.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  associate_public_ip_address = true
+  tags = {
+    Name = "Terraform1"
   }
 }
